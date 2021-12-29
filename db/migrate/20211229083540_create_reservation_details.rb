f4c7db034b487229a78600b99f17378fe61ccd79
Naoki Hagiwara <ne201183@senshu-u.jp>
2021-12-29 09:11:03 +0000

    明細と劇場モデルの作成

--- db/migrate/20211229083540_create_reservation_details.rb
+++ db/migrate/20211229083540_create_reservation_details.rb
@@ -2,6 +2,10 @@ class CreateReservationDetails < ActiveRecord::Migration[5.2]
   def change
     create_table :reservation_details do |t|
 
+
+
+      
+
       t.timestamps
     end
   end
--- db/migrate/20211229083608_create_theaters.rb
+++ db/migrate/20211229083608_create_theaters.rb
@@ -1,6 +1,9 @@
 class CreateTheaters < ActiveRecord::Migration[5.2]
   def change
     create_table :theaters do |t|
+      t.string :store_name, null: false
+      t.string :address, null: false
+      t.string :tel, null: false
 
       t.timestamps
     end

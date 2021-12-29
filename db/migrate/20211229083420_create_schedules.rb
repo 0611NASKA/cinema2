b5093971349a04fbf358f3aa2443c2b8cebfd198
Naoki Hagiwara <ne201183@senshu-u.jp>
2021-12-29 09:05:26 +0000

    scheduleとmovieモデルの作成

--- db/migrate/20211229083420_create_schedules.rb
+++ db/migrate/20211229083420_create_schedules.rb
@@ -2,6 +2,12 @@ class CreateSchedules < ActiveRecord::Migration[5.2]
   def change
     create_table :schedules do |t|
 
+
+
+      t.date :screening_date, null: false
+      t.time :starttime, null: false
+      t.time :endtime, null: false
+
       t.timestamps
     end
   end
--- db/migrate/20211229083434_create_movies.rb
+++ db/migrate/20211229083434_create_movies.rb
@@ -1,6 +1,13 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :movies do |t|
+      t.string :title, null: false
+      t.text :explanation, null: false
+      t.date :released_at, null: false
+      t.date :expired_at, null: false
+      t.integer :screening_time, null: false
+      t.string :distribution, null: false
+      t.string :director, null: false
 
       t.timestamps
     end

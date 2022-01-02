f49cd64b21f9b3b133fc59c7757804bddd49dfb7
Naoki Hagiwara <ne201183@senshu-u.jp>
2022-01-02 16:18:14 +0000

    モックアップの作成

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,11 @@
 class Movie < ApplicationRecord
   has_many :schedules, dependent: :destroy
+  has_one_attached :movie_picture
+  attribute :new_movie_picture
+
+  before_save do
+    if new_movie_picture
+      self.movie_picture = new_profile_picture
+    end
+  end
 end

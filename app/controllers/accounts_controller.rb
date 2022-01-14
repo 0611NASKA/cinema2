590cb407ddd8c9c36386c02b7ce3b745e66040fc
Naoki Hagiwara <ne201183@senshu-u.jp>
2022-01-14 00:33:22 +0000

    会員登録機能の追加

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,5 @@
 class AccountsController < ApplicationController
-  before_action :login_required
+  before_action :login_required, except: [:new, :create]
 
   def show
     @member = current_member
@@ -16,6 +16,23 @@ class AccountsController < ApplicationController
       redirect_to :account, notice: "アカウント情報を更新しました。"
     else
       render "edit"
+    end
+  end
+
+  def new
+    @member = Member.new(birthday: Date.new(1980, 1, 1))
+  end
+
+  def create
+    @member = Member.new(params[:member])
+    if @member.save
+      cookies.signed[:member_id] = {
+        value: @member.id,
+        expires: 10.minutes.from_now
+      }
+      redirect_to :root, notice: "会員を登録しました。"
+    else
+      render "new"
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     get "search", on: :collection
   end
   resource :session, only: [:create, :destroy]
-  resource :account, only: [:show, :edit, :update]
+  resource :account, except: :destroy
   resource :password, only: [:show, :edit, :update]
 end

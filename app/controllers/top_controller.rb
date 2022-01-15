e84093f3a824e092db24d74a9948af641fb02235
Naoki Hagiwara <ne201183@senshu-u.jp>
2022-01-15 07:07:10 +0000

    トップ画面の編集

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,5 @@
 class TopController < ApplicationController
   def index
+    @movies = Movie.order("id").limit(5)
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -18,4 +18,11 @@ class Movie < ApplicationRecord
       self.movie_picture = new_profile_picture
     end
   end
+
+  scope :visible, -> do
+    now = Time.current
+
+    where("released_at <= ?", now)
+      .where("expired_at > ? OR expired_at IS NULL", now)
+  end
 end

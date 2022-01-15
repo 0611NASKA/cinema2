6f9c38c0c2f8520b735b7bf6b9b878055f318ee7
Naoki Hagiwara <ne201183@senshu-u.jp>
2022-01-15 07:13:28 +0000

    ページネーションの実装

--- db/seeds/development/movies.rb
+++ db/seeds/development/movies.rb
@@ -4,16 +4,16 @@ body =
   "text、text、text、text、text、text、text、text、text、\n\n" +
   "text、text、text、text、text、text、text、text、text。"
 
-ftitles = ["進撃の", "襲撃の", "妄執の", "日常の"]
-gtitles = ["青春", "情報", "修羅場"]
+ftitles = ["進撃の", "襲撃の", "妄執の", "日常の", "愛の", "ただの"]
+gtitles = ["青春", "情報", "修羅場", "教室", "？？？"]
 int = [120, 130, 110]
 
 dis = ["生田", "神田", "石巻"]
 dir = ["田中", "佐藤", "堀北"]
 
-0.upto(9) do |idx|
+0.upto(19) do |idx|
   Movie.create(
-    title: "#{ftitles[idx % 4]}#{gtitles[idx % 3]}",
+    title: "#{ftitles[idx % 6]}#{gtitles[idx % 5]}",
     explanation: body,
     released_at: 31.days.ago.advance(days: idx),
     expired_at: 31.days.ago.advance(days: 60),
